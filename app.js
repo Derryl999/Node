b1539529267aed6de9b7e759910cd84a16b1b96c
Derryl999 <derryl.aranha999@gmail.com>
2020-06-30 20:06:49 +0530

    modules and require()

--- app.js
+++ app.js
@@ -1,20 +1,2 @@
-//passing function to another funtion
-function callFunction(func)
-{
-    func();
-}
-
-//normal function statement 
-function hello(){
-    console.log('hello world')
-
-}
-
-//function expression 
-var bye=function(){
-    console.log('good bye')
-}
-
-callFunction(bye)
-callFunction(hello)
-
+var c=require('./count')
+console.log(c([1,2,3,4,5,6,7,8]))
---
+++ count.js
@@ -0,0 +1,6 @@
+function counter(arr){
+    var len=arr.length
+    return 'there are '+len+' elements'
+
+}
+module.exports = counter;
