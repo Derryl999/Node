7c60a4d742013d1078a51caaf6d1c58c731fbd76
Derryl999 <derryl.aranha999@gmail.com>
2020-07-01 18:47:54 +0530

    creating and removing directories

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-//reading and writing files
 var fs = require("fs");
 
-//synchronous reading and writing
-
-var file=fs.readFileSync('test.txt','utf8')
-fs.writeFileSync('writeFile.txt',file)
-
-
-//asynchronous reading and writing
-
-fs.readFile("test.txt", "utf8", function (err, data) {
-  console.log(data);
-  fs.writeFile("abc.txt", data, (err) => {});
+//deleting a file
+fs.unlink("test.txt", (err) => {
+  if (err) console.log("may be file not found");
 });
-console.log("hello");
+
+//synchronous creating directory
+fs.mkdirSync("test_dir");
+
+//synchronous removing directory
+fs.rmdirSync("test_dir");
+
+//asynchronous creating directory
+fs.mkdir("test_dir", function () {
+  fs.readFile("test.txt", "utf8", function (err, data) {
+    fs.writeFile("./test_dir/writeMe.txt", data, (err) => {});
+  });
+});
+
+//removing file inside the directory and then  the directory itesef using async method
+fs.unlink("./test_dir/writeMe.txt", function () {
+  fs.rmdir("test_dir", (err) => {
+    console.log(err);
+  });
+});
