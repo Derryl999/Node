6b87994251b0049189825c51c048373c8eacf8c6
Derryl999 <derryl.aranha999@gmail.com>
2020-07-01 13:32:04 +0530

    reading and writing files

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
-var events = require("events");
-var util = require("util");
+//reading and writing files
+var fs = require("fs");
 
-/*var myEmitter = new events.EventEmitter();
-myEmitter.on("someEvent", function (msg) {
-  console.log(msg);
+//synchronous reading and writing
+
+var file=fs.readFileSync('test.txt','utf8')
+fs.writeFileSync('writeFile.txt',file)
+
+
+//asynchronous reading and writing
+
+fs.readFile("test.txt", "utf8", function (err, data) {
+  console.log(data);
+  fs.writeFile("abc.txt", data, (err) => {});
 });
-
-myEmitter.emit('someEvent','someEvent has emitted') */
-
-var Person = function (name) {
-  this.name = name;
-};
-
-util.inherits(Person, events.EventEmitter);
-
-var james = new Person("james");
-var july = new Person("july");
-var alice = new Person("alice");
-
-var people = [james, july, alice];
-
-people.forEach(function (person) {
-  person.on("speak", function (msg) {
-    console.log(person.name + " said " + msg);
-  });
-});
-james.emit("speak", "hello");
-july.emit("speak", "hey guys");
+console.log("hello");
