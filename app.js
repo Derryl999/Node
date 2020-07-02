7754739692892e28b7ea3392b8fadde8a4d0b0b2
Derryl999 <derryl.aranha999@gmail.com>
2020-07-02 23:20:35 +0530

    template engines and partial templates

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-var express=require('express')
-var app=express()
+var express = require("express");
+var app = express();
 
-app.get('/',function(req,res){
-  res.sendFile(__dirname+'/index.html')
-})
+app.set("view engine", "ejs");
 
-app.get('/contact',function(req,res){
-  res.sendFile(__dirname+'/contact.html')
-})
-app.get('/profile/:name',function(req,res){
-  // res.send('You are trying to access the profile of '+req.params.name)
- res.send(`You are trying to access the profile of ${req.params.name}`)
+app.get("/", function (req, res) {
+  res.render('index');
+});
 
-})
+app.get("/contact", function (req, res) {
+  res.render('contact')
+  // res.sendFile(__dirname + "/contact.html");
+});
+app.get("/profile/:name", function (req, res) {
+  var data={age:21,hobbies:["martial arts","singing","dancing","sleeping"],status:"student"}
+  res.render("profile",{person:req.params.name,data:data});
+});
 
-app.listen(3000)
+app.listen(3000);
