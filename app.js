3ad7c9659b665e25f1a34c9326b83e2203807689
Derryl999 <derryl.aranha999@gmail.com>
2020-07-03 01:01:31 +0530

    Middleware & Static Files

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+app.use('/assets',express.static('assets'));
+
 app.get("/", function (req, res) {
-  res.render('index');
+  res.render('index.ejs');
 });
 
 app.get("/contact", function (req, res) {
-  res.render('contact')
+  res.render('contact.ejs')
   // res.sendFile(__dirname + "/contact.html");
 });
 app.get("/profile/:name", function (req, res) {
