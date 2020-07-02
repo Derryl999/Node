0c631ca2df200ad0215ab09ff9573dec4ce0a79b
Derryl999 <derryl.aranha999@gmail.com>
2020-07-02 10:30:36 +0530

    Pipes

--- app.js
+++ app.js
@@ -4,8 +4,4 @@ var fs = require("fs");
 var myReadStream = fs.createReadStream(__dirname + "/test.txt", "utf8");
 var myWriteStream = fs.createWriteStream(__dirname + "/writeMe.txt");
 
-myReadStream.on("data", function (chunk) {
-  console.log("**************new chunk received******************");
-  // console.log(chunk);
-  myWriteStream.write(chunk);
-});
+myReadStream.pipe(myWriteStream)
