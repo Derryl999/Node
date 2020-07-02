1ae9ca138219f7c85bdd6374454fc6c502513bc7
Derryl999 <derryl.aranha999@gmail.com>
2020-07-02 10:53:02 +0530

    added pipes and server

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 var http = require("http");
 var fs = require("fs");
-
+/*
 var myReadStream = fs.createReadStream(__dirname + "/test.txt", "utf8");
 var myWriteStream = fs.createWriteStream(__dirname + "/writeMe.txt");
 
 myReadStream.pipe(myWriteStream)
 
+*/
 server = http.createServer(function (req, res) {
   console.log("user has made request to", req.url);
   res.writeHead(200, { ContentType: "text/plain" });
-  res.end(function(){
-    myReadStream.
-  });
+  var myReadStream = fs.createReadStream(__dirname + "/test.txt", "utf8");
+  myReadStream.pipe(res);
 });
+
 server.listen(3000, "localhost");
 console.log("server is listening to port 3000");
