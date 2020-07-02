030f057b0ba029810e3bb1be3e9fbbd3fcf32148
Derryl999 <derryl.aranha999@gmail.com>
2020-07-02 11:09:08 +0530

    serving html page

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 var http = require("http");
 var fs = require("fs");
-/*
-var myReadStream = fs.createReadStream(__dirname + "/test.txt", "utf8");
-var myWriteStream = fs.createWriteStream(__dirname + "/writeMe.txt");
 
-myReadStream.pipe(myWriteStream)
-
-*/
 server = http.createServer(function (req, res) {
   console.log("user has made request to", req.url);
-  res.writeHead(200, { ContentType: "text/plain" });
-  var myReadStream = fs.createReadStream(__dirname + "/test.txt", "utf8");
+  res.writeHead(200, { ContentType: "text/html" });
+  var myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
   myReadStream.pipe(res);
 });
 
