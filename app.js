030d9ed26cc91dbb92cdd9cfe160e91585a977e8
Derryl999 <derryl.aranha999@gmail.com>
2020-07-05 11:25:01 +0530

    handling POST requests

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
+var bodyParser=require('body-parser')
 
+
+var urlEncodedParser=bodyParser.urlencoded({extended:false})
 app.set("view engine", "ejs");
 
 app.use('/assets',express.static('assets'));
@@ -11,9 +14,15 @@ app.get("/", function (req, res) {
 
 app.get("/contact", function (req, res) {
   console.log(req.query)
-  res.render('contact.ejs',{qs:req.query})
+  res.render('contact',{qs:req.query})
   // res.sendFile(__dirname + "/contact.html");
 });
+
+app.post("/contact",urlEncodedParser, function (req, res) {
+  console.log(req.body)
+  res.render('contact-success',{qs:req.body})
+});
+
 app.get("/profile/:name", function (req, res) {
   var data={age:21,hobbies:["martial arts","singing","dancing","sleeping"],status:"student"}
   res.render("profile",{person:req.params.name,data:data});
